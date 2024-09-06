0561b6489c71704e1745fe852865665bfc71d49a
anonymous <nogolev6@gmail.com>
2024-09-06 12:48:26 +0300

    Миграция в базу данных модели Access Token

--- fastapi-app/core/models/__init__.py
+++ fastapi-app/core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "db_helper",
     "BaseModel",
     "User",
+    "AccessToken",
 )
 
 from .db_helper import db_helper
 from .base_model import BaseModel
 from .user import User
+from .access_token import AccessToken
