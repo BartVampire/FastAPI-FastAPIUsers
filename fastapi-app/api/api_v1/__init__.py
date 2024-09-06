db5d1e185c50179b5a066e6bdec184ee991a49a4
anonymous <nogolev6@gmail.com>
2024-09-06 18:49:07 +0300

    Подключение http_bearer

--- fastapi-app/api/api_v1/__init__.py
+++ fastapi-app/api/api_v1/__init__.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
+
 from .users import router as users_router
 from core.config import settings
 from .auth_router import router as auth_router
 
 """Подключаем модули и регистрируем маршруты в API V1"""
 
-router = APIRouter(prefix=settings.api.v1.prefix)
+http_bearer = HTTPBearer(auto_error=False)
+router = APIRouter(prefix=settings.api.v1.prefix, dependencies=[Depends(http_bearer)])
 
 router.include_router(
     users_router, prefix=settings.api.v1.users
