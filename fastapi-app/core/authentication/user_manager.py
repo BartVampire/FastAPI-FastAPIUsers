c6c6227cdc0d4d5e77dfcb0832384a1d9f267edf
anonymous <nogolev6@gmail.com>
2024-09-06 15:24:03 +0300

    Добавлена документация для UserManager

--- fastapi-app/core/authentication/user_manager.py
+++ fastapi-app/core/authentication/user_manager.py
@@ -15,15 +15,41 @@ log = logging.getLogger(__name__)
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
+    """
+    Класс UserManager для управления операциями пользователей. Наследуется от IntegerIDMixin и BaseUserManager.
+    """
+
     reset_password_token_secret = settings.access_token.reset_password_token_secret
     verification_token_secret = settings.access_token.verification_token_secret
 
     async def on_after_register(self, user: User, request: Optional["Request"] = None):
+        """
+        Записывает предупреждение в журнал после регистрации пользователя.
+
+        Параметры:
+        - user (User): Объект пользователя, который был зарегистрирован.
+        - request (Optional[Request]): Объект запроса FastAPI. По умолчанию None.
+
+        Возвращает:
+        - None
+        """
         log.warning("Пользователь зарегистрирован:", user.id)
 
     async def on_after_forgot_password(
         self, user: User, token: str, request: Optional["Request"] = None
     ):
+        """
+        Записывает предупреждение в журнал после того, как пользователь запросил сброс пароля.
+
+        Параметры:
+        - user (User): Объект пользователя, который запросил сброс пароля.
+        - token (str): Токен сброса пароля.
+        - request (Optional[Request]): Объект запроса FastAPI. По умолчанию None.
+
+        Возвращает:
+        - None
+        """
+
         log.warning(
             "Пользователь %r забыл свой пароль. Reset token: %r", user.id, token
         )
@@ -31,6 +57,18 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
     async def on_after_request_verify(
         self, user: User, token: str, request: Optional["Request"] = None
     ):
+        """
+        Записывает предупреждение в журнал после того, как пользователь запросил подтверждение по электронной почте.
+
+        Параметры:
+        - user (User): Объект пользователя, который запросил подтверждение по электронной почте.
+        - token (str): Токен подтверждения.
+        - request (Optional[Request]): Объект запроса FastAPI. По умолчанию None.
+
+        Возвращает:
+        - None
+        """
+
         log.warning(
             "Запрошено подтверждение для пользователя %r. Токен подтверждения: %r",
             user.id,
